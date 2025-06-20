342a2162c18c5daa8a08b249500ec34b34e44be5
vishal pathak <vishalpathak842@gmail.com>
2025-06-20 17:38:43 +0530

    Implement authentication controller with user registration, login, and info retrieval

---
+++ backend/controller/authController.js
@@ -0,0 +1,78 @@
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+import validator from "validator";
+import userModel from "../models/userModel.js";
+
+// Register User
+const registerUser = async (req, res) => {
+  try {
+    const { fullName, email, password, profileImageUrl } = req.body;
+
+    if (!fullName || !email || !password) {
+      return res.json({ success: false, message: "Missing Details" });
+    }
+
+    if (!validator.isEmail(email)) {
+      return res.json({ success: false, message: "Enter a valid email" });
+    }
+
+    if (password.length < 8) {
+      return res.json({ success: false, message: "Enter a strong password" });
+    }
+
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const user = await new userModel({
+      fullName,
+      email,
+      password: hashedPassword,
+      profileImageUrl,
+    }).save();
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+
+    res.json({ success: true, token });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: error.message });
+  }
+};
+
+// Login User
+const loginUser = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await userModel.findOne({ email });
+
+    if (!user) {
+      return res.json({ success: false, message: "User does not exist" });
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.json({ success: false, message: "Invalid credentials" });
+    }
+
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+    res.json({ success: true, token });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: error.message });
+  }
+};
+
+// get user Info
+const getUserInfo = async (req, res) => {
+  try {
+    const user = await userModel.findById(req.user.id).select("-password");
+    if (!user) {
+      return res.json({ success: false, message: "User not found" });
+    }
+     res.json({ success: true, user });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: error.message });
+  }
+};
+
+export { registerUser, loginUser, getUserInfo };
