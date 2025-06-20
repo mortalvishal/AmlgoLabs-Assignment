de4360125bb849ff976f6a07ae29480f7314934f
vishal pathak <vishalpathak842@gmail.com>
2025-06-20 17:38:21 +0530

    Add user router and configure API endpoints

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from 'express'
 import cors from 'cors'
 import 'dotenv/config'
 import connectDB from './config/db.js'
+import userRouter from './routes/authRoutes.js'
 
 
 // App config
@@ -13,9 +14,14 @@ connectDB()
 app.use(express.json())
 app.use(cors())
 
+// API Endpoints
+app.use('/api/user', userRouter)
 
 app.get('/',(req,res)=>{
     res.send('API working')
 })
+
+// Server uploads folder
+app.use("/uploads", express.static(Path.join(__dirname,"uploads")));
 
 app.listen(port, ()=> console.log("Server Started",port))
