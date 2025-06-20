cea0aa532f8bb025d9355d198f9de1b5554ffd97
vishal pathak <vishalpathak842@gmail.com>
2025-06-21 00:53:05 +0530

    Fix income routes to use PDF download instead of Excel

--- backend/routes/incomeRoutes.js
+++ backend/routes/incomeRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import {addIncome, getAllIncome, deleteIncome, downloadIncomeExcel } from '../controller/incomeController.js'
+import {addIncome, getAllIncome, deleteIncome, downloadIncomePDF } from '../controller/incomeController.js'
 import protect from '../middleware/authMiddleware.js'
 
 
@@ -7,7 +7,7 @@ const incomeRouter = express.Router();
 
 incomeRouter.post("/add", protect, addIncome);
 incomeRouter.get("/get", protect, getAllIncome);
-incomeRouter.get("/download-excel", protect, downloadIncomeExcel)
+incomeRouter.get("/download-pdf", protect, downloadIncomePDF)
 incomeRouter.delete("/:id", protect, deleteIncome)
 
 export default incomeRouter;
